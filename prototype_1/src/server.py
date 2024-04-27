34d0d1d3cecd6ab5f35e0b5f9891adc5927be2e8
Moonflower2022 <104652800+Moonflower2022@users.noreply.github.com>
2024-04-27 07:48:04 -0700

    oops accidentally changed smth in other file

--- prototype_1/src/server.py
+++ prototype_1/src/server.py
@@ -14,7 +14,8 @@ CASH_COLORS = [
 chart = mesa.visualization.ChartModule(
     [
         {"Label": "Bank Money", "Color": LOANS_COLOR},
-        {"Label": "Total Cash", "Color": TOTAL_CASH_COLOR}
+        {"Label": "Total Cash", "Color": TOTAL_CASH_COLOR},
+        {"Label": "Total Deposits", "Color": TOTAL_DEPOSITS_COLOR}
     ]
 )
 
